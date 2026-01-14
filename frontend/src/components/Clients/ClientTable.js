06c099c58bc889d2baae616b71bb75396e5ab349
Yulian Soracá <soracayulian@gmail.com>
2026-01-14 14:59:49 -0500

    Restablecimiento de la tabla eliminada de clientes, ahora más responsive

--- frontend/src/components/Clients/ClientTable.js
+++ frontend/src/components/Clients/ClientTable.js
@@ -18,7 +18,60 @@ function ClientTable() {
       vence: '2024/06/03',
       Estado: 'Cancelado',
       Dias: '18'
+    },
+    {
+      id: 2,
+      Cédula: '112212132',
+      Nombre: 'Agustin Rojas',
+      Telefono: '3202552456',
+      Eps: 'Capital Salud',
+      Rh: ' O-',
+      Plan: 'Mensualidad',
+      Inicio: '2024/05/01',
+      vence: '2024/06/03',
+      Estado: 'Cancelado',
+      Dias: '18'
+    },
+    {
+      id: 1,
+      Cédula: '112212132',
+      Nombre: 'Agustin Rojas',
+      Telefono: '3202552456',
+      Eps: 'Capital Salud',
+      Rh: ' O-',
+      Plan: 'Mensualidad',
+      Inicio: '2024/05/01',
+      vence: '2024/06/03',
+      Estado: 'Cancelado',
+      Dias: '18'
+    },
+    {
+      id: 1,
+      Cédula: '112212132',
+      Nombre: 'Agustin Rojas',
+      Telefono: '3202552456',
+      Eps: 'Capital Salud',
+      Rh: ' O-',
+      Plan: 'Mensualidad',
+      Inicio: '2024/05/01',
+      vence: '2024/06/03',
+      Estado: 'Cancelado',
+      Dias: '18'
     }
+    ,{
+      id: 1,
+      Cédula: '112212132',
+      Nombre: 'Agustin Rojas',
+      Telefono: '3202552456',
+      Eps: 'Capital Salud',
+      Rh: ' O-',
+      Plan: 'Mensualidad',
+      Inicio: '2024/05/01',
+      vence: '2024/06/03',
+      Estado: 'Cancelado',
+      Dias: '18'
+    }
+
   ];
   const getEstadoColor = (Estado) => {
     switch(Estado.toLowerCase()) {
@@ -34,38 +87,43 @@ function ClientTable() {
     borderRadius: '0em 0em 1em 1em',
      padding: '1em',
      margin:'5em 8em 0em 8em' }}>
-      <h2 className='TableTitle' style={{
-        fontSize: '20px',
 
-        margin: '0px 90px 30x 0px',
+    <div className='headBorder'>
+
+       <h2 className='TableTitle' style={{
+        fontSize: '1.4rem',
+        margin: '0em 1em 0em 0em',
         fontWeight: '600',
-        marginBottom: '25px',
-
-        color: '#2c3e50',
-        paddingBottom: '10px'
+        display:'inline',
+        whiteSpace:'nowrap',
+        color: '#2683ff',
+        paddingRight:'2em',
       }}>Gestión de Clientes</h2>
-      
+
+      {/*Botón Añadir cliente */}
+      <button className='AddClient' >+  Añadir</button>
+
+
+    </div>
+     
+
       {/* Barra de búsqueda */}
    <div className='searchClient' >
-        <input type='text'  className='searchC' placeholder='🔍  Buscar cliente...' 
-        style={{ backgroundColor:'whitesmoke',
-        border:'1px solid grey',
-        margin:'0vh 0vh 3vh 8vh', 
-        fontSize:'15px', 
-        fontFamily:'system-ui',
-         borderRadius:'23px'}} />
+        <input type='text'
+          className='searchC' placeholder='🔍  Buscar cliente...' 
+        />
     
       </div>
 
 
       
       {/* Tabla */}
-      <div>
-        <table>
-          <thead >
-            <tr className='theadTitle'>
+      <div className='ContainerTable'>
+        <table className='TableScroll'>
+          <thead className='theadHeader' >
+            <tr>
               <th className='idTable'>id</th>
-              <th>Cédula</th>
+              <th >Cédula</th>
               <th>Nombre</th>
               <th>Telefono</th>
               <th>Eps</th>
@@ -78,18 +136,18 @@ function ClientTable() {
               <th className='accionTable'>Accion</th>
             </tr>
           </thead>
-          <tbody>
+          <body>
             {clients.map((client, index) => (
-              <tr key={index} style={{
-                borderBottom: '1px solid #eee',
-                transition: 'background 0.2s'
+              <tr key={index} className='filaTable' style={{
+                borderBottom: '11px solid #fff',
+                transition: 'background 0.2s',
               }}>
                 <td style={{ padding: '12px 15px' }}>{client.id}</td>
                 <td style={{ padding: '12px 15px' }}>{client.Cédula}</td>
                 <td style={{ padding: '12px 15px' }}>{client.Nombre}</td>
                 <td style={{ padding: '12px 15px' }}>{client.Telefono}</td>
                 <td style={{ padding: '12px 15px' }}>{client.Eps}</td>
-                <td style={{ padding: '12px 15px' }}>{client.Rh}</td>
+                <td style={{ padding: '8px 10px' }}>{client.Rh}</td>
                 <td style={{ padding: '12px 15px' }}>{client.Plan}</td>
                 <td style={{ padding: '12px 15px' }}>{client.Inicio}</td>
                 <td style={{ padding: '12px 15px' }}>{client.vence}</td>
@@ -100,22 +158,24 @@ function ClientTable() {
                     backgroundColor: getEstadoColor(client.Estado),
                     marginBottom:'23px',
                     color: '#f5f7fa',                    
-                    padding: '2px 8px',
+                    padding: '0.2rem 0.3rem',
                     borderRadius: '20px',
-                    fontSize: '15px',
+                    fontSize: '13px',
                     fontWeight: '550'
                   }}>
                     {client.Estado}
                   </span>
                 </td>
-                <td style={{ padding: '12px 11px' }}>
+                <td> <span><td>
                   <button style={{
                     backgroundColor: '#fe5221',
                     color: 'white',
                     border: 'none',
-                    padding: '6px 12px',
+                    padding: '0.45rem 0.66rem',
                     borderRadius: '6px',
                     cursor:'pointer',
+                    marginRight:'0.40rem',
+                    marginLeft:'0.30rem',
                     fontSize:'12px',
                     transition: 'background-color 0.3s'
                   }}
@@ -124,12 +184,12 @@ function ClientTable() {
                     Eliminar
                   </button>
                 </td>
-                <td style={{ padding: '12px 15px' }}>
+                <td >
                   <button style={{
                     backgroundColor: '#3498db',
                     color: 'white',
                     border: 'none',
-                    padding: '6px 12px',
+                     padding:'0.45rem 0.66rem',
                     borderRadius: '6px',
                     cursor: 'pointer',
                     fontSize: '12px',
@@ -140,9 +200,11 @@ function ClientTable() {
                     Editar
                   </button>
                 </td>
+                </span>
+                 {client.Accion}</td>
               </tr>
             ))}
-          </tbody>
+          </body>
         </table>
       </div>
     </div>
